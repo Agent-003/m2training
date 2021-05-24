9219f0a3b4bb561768b4b890b177021a408d88cc
Agent-003 <abbasova003@gmail.com>
2021-05-24 13:20:42 +0300

    Task 1.10.
    Add
    - app/etc/config.php
    
    Fix
    - Объект сессии получаем через конструктор.
    - Повесили обсервер на событие controller_action_predispatch_catalog_product_view

--- app/code/Training/TestObservers/Observer/RedirectToLogin.php
+++ app/code/Training/TestObservers/Observer/RedirectToLogin.php
@@ -8,6 +8,11 @@ class RedirectToLogin implements ObserverInterface
 {
 
     /**
+     * @var \Magento\Customer\Model\Session
+     */
+    private $customerSession;
+
+    /**
      * @var \Magento\Framework\App\Response\RedirectInterface
      */
     private $redirect;
@@ -24,24 +29,23 @@ class RedirectToLogin implements ObserverInterface
      */
     public function __construct(
         \Magento\Framework\App\Response\RedirectInterface $redirect,
+        \Magento\Customer\Model\Session $customerSession,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->redirect = $redirect;
+        $this->customerSession = $customerSession;
         $this->logger = $logger;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
-
         $request = $observer->getEvent()->getData('request');
 
         $this->logger->debug($request->getModuleName());
         $this->logger->debug($request->getControllerName());
         $this->logger->debug($request->getActionName());
 
-        if (!$customerSession->isLoggedIn()) {
+        if (!$this->customerSession->isLoggedIn()) {
             if (
                 $request->getModuleName() == 'catalog'
                 && $request->getControllerName() == 'product'
