d23e5664ee4d351c25bc44ba614db8531fb54bd6
Agent-003 <abbasova003@gmail.com>
2021-05-28 23:16:21 +0300

    Task 4.2

---
+++ app/code/Training/TestJs/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/catalog-add-to-cart': {
+                'Training_TestJs/js/add-to-card-mixin': true
+            }
+        }
+    }
+};
